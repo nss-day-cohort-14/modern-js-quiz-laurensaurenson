a41166f524909195ae469ef3ea797d4cc0a9e044
Lauren Smith <laurensmith@comcast.net>
2016-07-12 10:33:14 -0500

    Make Them Hit Each Other

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ let Dragon = new BattleBots.Robot.DragonBot();
 let robot1;
 let robot2;
 
+let fightRobot1 = $("#fightRobot1");
+let fightRobot2 = $("#fightRobot2");
+
 let nameInput1 = $("#name1");
 let nameInput2 = $("#name2");
 let typeSelects = $(".robot-types");
@@ -33,7 +36,27 @@ let fillPlayers = () => {
   robot2.playerName = nameInput2.val();
 
   showAttackScreen();
-}
+  fillAttackScreen(robot1, fightRobot1);
+  fillAttackScreen(robot2, fightRobot2);
+};
+
+let attack = () => {
+  robot1.health = robot1.health - robot2.damage;
+  robot2.health = robot2.health - robot1.damage;
+  fillAttackScreen(robot1, fightRobot1);
+  fillAttackScreen(robot2, fightRobot2);
+};
+
+// fill attack screen information //
+let fillAttackScreen = (robot, robotBox) => {
+  robotBox.empty();
+  let playerName = $("<h4>").html(`Player Name: ${robot.playerName}`);
+  let robotType = $("<h4>").html(`Robot Type: ${robot.name}`);
+  let robotHealth = $("<h4>").html(`Health: ${robot.health}`);
+  let robotAttack = $("<h5>").html(`Attack: ${robot.attack}`);
+  let robotDamage = $("<h5>").html(`Damage: ${robot.damage}`);
+  robotBox.append(playerName, robotType, robotHealth, robotAttack, robotDamage);
+};
 
 // switches to Attack View //
 let showAttackScreen = () => {
@@ -41,7 +64,7 @@ let showAttackScreen = () => {
   $("#fightReady").addClass("hidden");
   $("#makeRobot1").addClass("hidden");
   $("#makeRobot2").addClass("hidden");
-}
+};
 
 let fightReady = $("#fightReady").click(fillPlayers);
-// let attackButton = $("#attackButton").click(fillPlayers);
+let attackButton = $("#attackButton").click(attack);
